33da12882c3742eaa0b8c46eaecf7e42f094972d
hafizulloh-front <boqijonovhafizulloh@gmail.com>
2026-05-16 15:33:45 +0500

    progress

--- script.js
+++ script.js
@@ -76,3 +76,52 @@
 // } 
 
 // console.log(findMax([1, 5, 3]));
+
+//=========================6
+
+// let talabalar = [
+//     {
+//         id: 1,
+//         name: "Ali",
+//         age: 16,
+//         score: 85
+//         }
+        
+// ]
+
+// function yangiTalabaQosh(talaba) {
+//     const id=talabalar.length ? talabalar[talabalar.length - 1].id +1:1;
+//     talaba.id=id;
+//     talabalar.push(talaba);
+//     console.log("yangi talaba qoshildi:", talaba);
+    
+// }
+
+// function talabalarniKor() {
+//     console.log("hozirgi talabalar:");
+//     console.log(talabalar);
+    
+// }
+
+// function talabaniYangila(id, yangilanganMalumot) {
+//     let talaba=talabalar.find(t => t.id==id);
+//     if(talaba) {        
+//         Object,assign(talaba, yangilanganMalumot);
+//         console.log("talaba yangilandi:", talaba);   
+//     }else {
+//         console.log("talaba topilmadi");   
+//     }
+// }
+
+// function talabaOchir(id) {
+//     let index = talabalar.findIndex(t=>t.id==id);
+//     if (index!==-1) {
+//         let ochirilgan=talabalar.splice(index,1);
+//         console.log('talaba ochirildi:', ochirilgan[0]);   
+//     }else{
+//         console.log("talaba topilmadi");
+        
+//     }
+// }
+
+// yangiTalabaQosh({ism:"husan", yosh:19, baho:91});
