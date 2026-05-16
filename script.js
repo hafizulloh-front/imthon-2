1d637578ec5b593625cd8f7fd9a7e0fc97f57212
hafizulloh-front <boqijonovhafizulloh@gmail.com>
2026-05-16 14:13:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//=========================1
+// function sumTo(n) {
+//     if(n <=0) {
+//         return 0;
+//     }
+
+//     let sum = 0;
+//     for(let i = 1; i<=n; i++) {
+//         sum +=i;
+//     }
+
+//     return sum; 
+    
+// }
+
+// console.log(sumTo(5));
+
+//=====================2
